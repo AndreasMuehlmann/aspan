4446ff21032447ce8f5ba4f62eab313c8f87cc5f
andi <andi.muehlmann@arcor.de>
2022-01-29 12:06:05 +0100

    upper_limit_y_axis can be changed

--- README.md
+++ README.md
@@ -8,35 +8,41 @@ wavelength_to_RGB: http://www.noah.org/wiki/Wavelength_to_RGB_in_Python
 
 Quickstart:
 
-    To enter a spectrum make a new text file and enter the points of the curve.
-        - one point is on one line
-        - x and y koordinates are separated by a comma
-        - x is first y is second
-        - when entering floating pont numbers use a dot (example: 2.5)
-        - the first point has to have the x value 380 and the last x value has to be 780
-        - the absorption (the y) has to be measured from 0 to 2.5
-            (
-                If yours is measured diffenrently use the convert.py.
-                For that enter "python convert.py [filename] (of the spectrum) [faktor] (to multiply your y so its from 0 to 2.5)"
-                (you have to be in the directory of the  convert.py and the spectrum)
-            )
+    To enter a spectrum:
+        - make a new text file
+        - write your upper limit for the y axis into the first line
 
-        it should look like this:
-        x1, y1
-        x2, y2
-        x3, y3
-        x4, y4
-        x5, y5
-        x6, y6
+        To enter the points:
+            - one point is on one line
+            - x and y koordinates are separated by a comma
+            - x is first y is second
+            - when entering floating pont numbers use a dot (example: 2.5)
+            - the first point has to have the x value 380 and the last x value has to be 780
+            - the absorption (the y) has to be measured from 0 to 2.5
+                (
+                    If yours is measured diffenrently use the convert.py.
+                    For that enter "python convert.py [filename] (of the spectrum) [faktor] (to multiply your y so its from 0 to 2.5)"
+                    (you have to be in the directory of the  convert.py and the spectrum)
+                )
 
-        example
-        380.0, 1.0556
-        420.0, 1.9167
-        460.0, 0.2778
-        640.0, 0.3333
-        675.0, 1.5278
-        700.0, 0.4167
-        780.0, 0.2778
+            it should look like this:
+            faktor
+            x1, y1
+            x2, y2
+            x3, y3
+            x4, y4
+            x5, y5
+            x6, y6
+
+            example
+            2.5
+            380.0, 1.0556
+            420.0, 1.9167
+            460.0, 0.2778
+            640.0, 0.3333
+            675.0, 1.5278
+            700.0, 0.4167
+            780.0, 0.2778
 
     after finishing:
 
--- main.py
+++ main.py
@@ -11,6 +11,15 @@ pygame.init()
 def read_points(file):
     points = []
     for line_number, line in enumerate(file):
+
+        if line_number == 0:
+            list_for_line = line.strip().split()
+            list_for_line[0] = list_for_line[0].replace(' ', '')
+            assert (list_for_line[0].replace('.','',1).isdigit() and list_for_line[0].count('.') < 2),\
+                f'In the first line must be the upper limit of the y axis.'
+            upper_limit_y_axis = float(list_for_line.pop())
+            continue
+
         point = line.strip().split(',')
         assert len(point) == 2, f'A point has to be separated by a comma and contains only two numbers.\
             There can only be one point per line. (line: {line_number + 1})'
@@ -31,7 +40,7 @@ def read_points(file):
 
     points.sort(key=lambda point: point[0])
     assert 375 <= points[0][0] <= 385 and 775 <= points[-1][0] <= 785, 'The first x should be around 380 (+-5) and the last x should be around 755 (+-5).'
-    return points
+    return points, upper_limit_y_axis
 
 def calc_slope(point1, point2):
     if (point2[0] - point1[0]) == 0:
@@ -67,8 +76,8 @@ def get_area_under_curve(points, precision):
 
     return round(area_left, 2), round(area_right, 2)
 
-def get_total_area(points):
-    return round((points[-1][0] - points[0][0]) * 2.5, 2)
+def get_total_area(points, upper_limit_y_axis):
+    return round((points[-1][0] - points[0][0]) * upper_limit_y_axis, 2)
 
 def get_point_max_min(points):
     point_max_absorbtion = points[0]
@@ -88,19 +97,19 @@ def get_point_max_min(points):
 def get_average_absorbtion(points, area_not_absorbed):
     return round(area_not_absorbed / (points[-1][0] - points[0][0]), 2)
 
-def get_average_wave_length(points, area_left, area_right):
-    return round((points[0][0] + points[-1][0]) / 2 - (area_left / 2.5) + (area_right / 2.5), 2)
+def get_average_wave_length(points, area_left, area_right, upper_limit_y_axis):
+    return round((points[0][0] + points[-1][0]) / 2 - (area_left / upper_limit_y_axis) + (area_right / upper_limit_y_axis), 2)
 
-def get_share(points, area):
-    return round(area / get_total_area(points), 2)
+def get_share(points, area, upper_limit_y_axis):
+    return round(area / get_total_area(points, upper_limit_y_axis), 2)
 
-def get_saturation(y_max, y_min):
-    return round((y_max - y_min) / 2.5, 2)
+def get_saturation(y_max, y_min, upper_limit_y_axis):
+    return round((y_max - y_min) / upper_limit_y_axis, 2)
 
 def main():
     assert len(sys.argv) == 2, f'The programm takes one argument, a file to read points from. {len(sys.argv)} arguments where given'
     with open(sys.argv[1]) as file:
-        points = read_points(file)
+        points, upper_limit_y_axis = read_points(file)
 
     #the area_under_curve of test points is 4
     #points = [[0, 0], [2, 2], [4, 0]]
@@ -111,19 +120,19 @@ def main():
     area_under_curve = area_left_under_curve + area_right_under_curve
     print(f'\narea under curve: {area_under_curve}')
 
-    area_left_above_curve = round((get_total_area(points) / 2) - area_left_under_curve, 2)
-    area_right_above_curve = round((get_total_area(points) / 2) - area_right_under_curve, 2)
+    area_left_above_curve = round((get_total_area(points, upper_limit_y_axis) / 2) - area_left_under_curve, 2)
+    area_right_above_curve = round((get_total_area(points, upper_limit_y_axis) / 2) - area_right_under_curve, 2)
 
     area_above_curve = area_left_above_curve + area_right_above_curve
     print(f'\narea above curve: {area_above_curve}')
 
-    average_wave_length_under_curve = get_average_wave_length(points, area_left_under_curve, area_right_under_curve)
+    average_wave_length_under_curve = get_average_wave_length(points, area_left_under_curve, area_right_under_curve, upper_limit_y_axis)
     print(f'\naverage wave length above curve: {average_wave_length_under_curve}')
 
-    average_wave_length_above_curve = get_average_wave_length(points, area_left_above_curve, area_right_above_curve)
+    average_wave_length_above_curve = get_average_wave_length(points, area_left_above_curve, area_right_above_curve, upper_limit_y_axis)
     print(f'\naverage wave length above curve: {average_wave_length_above_curve}')
 
-    intensity = get_share(points, area_above_curve)
+    intensity = get_share(points, area_above_curve, upper_limit_y_axis)
     print(f'\nintensity: {intensity}')
 
     point_max, point_min = get_point_max_min(points)
@@ -131,12 +140,11 @@ def main():
     print(f'x of minimum: {point_min[0]}, y of minimum: {point_min[1]}\n')
 
 
-    saturation = get_saturation(point_max[1], point_min[1])
+    saturation = get_saturation(point_max[1], point_min[1], upper_limit_y_axis)
     print(f'\nsaturation: {saturation}')
 
 #   average_absorbtion = get_average_absorbtion(points, area_under_curve)
 #   print(f'\navergae absorbtion: {average_absorbtion}\n')
-#
 
     visible_color = wavelength_to_rgb(average_wave_length_above_curve, intensity, saturation)
 
